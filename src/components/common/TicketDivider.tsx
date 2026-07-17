5a93e556b081dc54425f251423ddf61eb8160fa9
Vinay-1206 <svinaygoud12@gmail.com>
2026-07-17 16:39:10 +0530

    ticketDivider UI updated

--- src/components/common/TicketDivider.tsx
+++ src/components/common/TicketDivider.tsx
@@ -1,8 +1,8 @@
 export default function TicketDivider() {
   return (
     <div className="relative -mx-6 my-1 flex w-[calc(100%+3rem)] items-center">
-      <span className="absolute -left-3 top-1/2 h-6 w-6 -translate-y-1/2 rounded-full bg-background" />
-      <span className="absolute -right-3 top-1/2 h-6 w-6 -translate-y-1/2 rounded-full bg-background" />
+      <span className="absolute -left-3 top-1/2 h-6 w-6 -translate-y-1/2 rounded-full bg-[var(--ticket-divider-cutout)]" />
+      <span className="absolute -right-3 top-1/2 h-6 w-6 -translate-y-1/2 rounded-full bg-[var(--ticket-divider-cutout)]" />
       <div className="mx-6 h-0 w-full border-t-2 border-dashed border-black/15" />
     </div>
   );
